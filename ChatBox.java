32446b7cc93e4afad4470e1f35557ab5e3ddb74a
Roni Diwan <ronidiwan@gmail.com>
2020-02-09 22:20:18 -0800

    GUI

--- ChatBox.java
+++ ChatBox.java
@@ -7,35 +7,49 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class ChatBox extends JPanel implements ActionListener{
-
+	
 		private JButton cmdSend;
-		private JTextArea text;
+		private JTextField text;
 		private JPanel input;
+		private JTextArea output;
+		private String userName;
+		private String txt;
 		
-		private JTextArea chat;
-		
-		public ChatBox()
+		public ChatBox(String name)
 		{
+			userName = name;
 			cmdSend = new JButton ("Send");
 			cmdSend.addActionListener(this);
-			text = new JTextArea();
+			text = new JTextField();
+			input = new JPanel(new BorderLayout());
+			this.setLayout(new BorderLayout());
 			
 			//chat window area
-			chat = new JTextArea();
-			JScrollPane scroll = new JScrollPane();
-			scroll.add(chat);
+			output = new JTextArea(30,50);
+			JScrollPane scroll = new JScrollPane(output);
 			
 			//add button and text field to input panel
 			input.add(cmdSend, BorderLayout.EAST);
 			input.add(text, BorderLayout.CENTER);
 			
 			this.add(input, BorderLayout.SOUTH);
+			this.add(scroll, BorderLayout.CENTER);
 		}
 		
 		
 		public void showInChat(String s)
 		{
-			text.append(s);
+			output.append(userName + ": " + s + "\n");
+		}
+		
+		public void welcome()
+		{
+			output.append(userName + " has enterd chat!\n");
+		}
+		
+		public String getText()
+		{
+			return txt;
 		}
 
 
@@ -44,8 +58,10 @@ public class ChatBox extends JPanel implements ActionListener{
 		{
 			if (e.getSource() == cmdSend)
 			{
-				String s = text.getText();
-				showInChat(s);
+				txt = text.getText();
+				showInChat(txt);
+				text.setText("");
+				txt = "";
 			}
 					
 		}
--- Client.java
+++ Client.java
@@ -12,13 +12,17 @@ public class Client {
 	
 	private void startChat()
 	{
+        //Set a user name
+        String userName = JOptionPane.showInputDialog(null, "Enter user name:");
+        
 		//GUI
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500,500);
-		ChatBox box = new ChatBox();
+		ChatBox box = new ChatBox(userName);
 		
 		frame.add(box);
+		frame.pack();
 		frame.setVisible(true);
 		
 				
@@ -27,24 +31,23 @@ public class Client {
         BufferedReader in = null;
         String host = "localhost";
         
-        //Set a user name
-        String userName = JOptionPane.showInputDialog(null, "Enter user name:");
-        
         
         try{
 	        	socket = new Socket(host, 7777);
 	            out = new PrintWriter(socket.getOutputStream(), true);
 	            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	   
-	            box.showInChat( userName + " enterd chat");
-	            String s = JOptionPane.showInputDialog(null, "Enter a string to send:");
+	            //in = new InputStreamReader(socket.getInputStream());
+	            
+	            box.welcome();
+	            //String s = JOptionPane.showInputDialog(null, "Enter a string to send:");
+	            String input = box.getText();
 	            
 	            //while receiving input, print it on the screen and wait for next
-	            while(s != null)
+	            while(in != null)
 	            {
-	                out.println(s); //print in server
-	                System.out.println(userName + ": " + in.readLine()); //print in client
-	                s = JOptionPane.showInputDialog(null, "Enter a string to send:");
+	                out.println(input); //print in server
+	                box.showInChat(""+in.read()); //print in client
+	                //s = JOptionPane.showInputDialog(null, "Enter a string to send:");
 	            }
 	            
 	            //close all streams
